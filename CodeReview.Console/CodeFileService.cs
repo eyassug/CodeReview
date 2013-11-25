a3bc829d376170b84d5688e6c1ea41dbe293b077
Eyassu Getachew <eyassug@gmail.com>
2013-11-25 12:22:26 +0300

    Added ComparerViewModel and CodeFileService (ICodeFileService implementation) to Console application

---
+++ CodeReview.Console/CodeFileService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeReview.Services;
+using Roslyn.Compilers.CSharp;
+
+namespace CodeReview.Console
+{
+    class CodeFileService : ICodeFileService
+    {
+        public CodeFile Create(string filePath)
+        {
+            return new CodeFile(SyntaxTree.ParseFile(filePath));
+        }
+    }
+}
---
+++ CodeReview.Console/ComparerViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeReview.Services;
+
+namespace CodeReview.Console
+{
+    class ComparerViewModel
+    {
+        #region Fields
+
+        private readonly ICodeFileService _codeFileService;
+        private IClassComparerService _classComparerService;
+        private string _sourceFilePath;
+        private string _refactoredFilePath;
+        private CodeFile _baseCodeFile;
+        private CodeFile _refactoredCodeFile;
+        #endregion
+
+
+        #region Constructors
+        
+        public ComparerViewModel()
+        {
+            _codeFileService = new CodeFileService();
+        }
+
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+        public void Compare()
+        {
+            _baseCodeFile = _codeFileService.Create(File1);
+            _refactoredCodeFile = _codeFileService.Create(File2);
+            var result = _classComparerService.Compare(_baseCodeFile.Classes.First(),
+                                                       _refactoredCodeFile.Classes.First());
+
+        }
+
+        public bool CanCompare()
+        {
+            return true;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string File1
+        {
+            get { return _sourceFilePath; }
+            set
+            {
+                if(_sourceFilePath == value)
+                    return;
+                _sourceFilePath = value;
+            }
+        }
+
+        public string File2
+        {
+            get { return _refactoredFilePath; }
+            set
+            {
+                if (_refactoredFilePath == value)
+                    return;
+                _refactoredFilePath = value;
+            }
+        }
+
+
+
+
+        #endregion
+
+    }
+}
