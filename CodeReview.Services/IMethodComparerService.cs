3cd846e8b2bfc08011d41a69513d0a945917a2f1
Eyassu Getachew <eyassug@gmail.com>
2013-11-28 16:08:23 +0300

    Added Method signatures on IMethodComparerService

--- CodeReview.Services/IMethodComparerService.cs
+++ CodeReview.Services/IMethodComparerService.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using CodeReview.Services.Comparison;
 
 namespace CodeReview.Services
 {
     public interface IMethodComparerService
     {
-        MethodComparisonResult Compare(Method method1, Method method2);
+        MethodComparisonResultBase Compare(Method baseMethod, Method refactoredMethod);
+        Method FindMatchingOverload(Method method, ICollection<Method> methodCollection);
+        IList<string> GetRemovedLines(Method baseMethod, Method refactoredMethod);
+        IList<string> GetAddedLines(Method baseMethod, Method refactoredMethod);
     }
 }
