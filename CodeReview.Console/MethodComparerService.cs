689363fc03dd86f2323ce0fc732b44bc4ce9eae5
Eyassu Getachew <eyassug@gmail.com>
2013-11-28 16:45:36 +0300

    Added IMethodComparerService implementation

---
+++ CodeReview.Console/MethodComparerService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeReview.Services;
+using CodeReview.Services.Comparison;
+
+namespace CodeReview.Console
+{
+    class MethodComparerService : IMethodComparerService
+    {
+        public MethodComparisonResultBase Compare(Method baseMethod, Method refactoredMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Method FindMatchingOverload(Method method, IList<Method> methodCollection)
+        {
+            var originalParameterSet = method.Parameters.Aggregate("", (current, parameter) => current + parameter.Type);
+            foreach (var methodOverload in methodCollection)
+            {
+                var overloadParameterSet = methodOverload.Parameters.Aggregate("", (current, parameter) => current + parameter.Type.ToString());
+                if (overloadParameterSet == originalParameterSet)
+                    return methodOverload;
+            }
+            return null;
+        }
+
+        public IList<string> GetRemovedLines(Method baseMethod, Method refactoredMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetAddedLines(Method baseMethod, Method refactoredMethod)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
